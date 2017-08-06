df8901c37bbce84a4f87586578ecf98b5a7a05e8
avner ainouz <aainouz@univ.haifa.ac.il>
2017-08-06 23:24:35 +0300

    fix source

--- public/app/controllers/superUserCtrl.js
+++ public/app/controllers/superUserCtrl.js
@@ -1,31 +1,41 @@
 angular
         .module('app')
-        .controller('superUserCtrl', function ($scope,$firebaseArray) {
+        .controller('superUserCtrl', function ($scope, $firebaseArray) {
 
 
             $scope.users = [];
-
+            $scope.selectedItem = null;
+            $scope.searchText = null;
+          
             var UsersRef = firebase.database().ref('users');
 
             $scope.updateUsers = function (searchText) {
+                console.clear();
+                console.log(searchText);
                 console.log('snapshot');
-                var query = UsersRef.orderByChild('phone').startAt(searchText).once('value', function(snap) {
-       console.log('accounts matching email address', snap.val());
-    });
-    
-      console.log(query);
+                var query = $firebaseArray(UsersRef
+                        .orderByChild('phone')
+                        .startAt(searchText)
+                        .endAt(searchText + "\uf8ff"));
                 
-                
+//                        .on('child_added', function (data) {
+//                            $scope.users = data.val();
+//                            console.log($scope.users);
+//                        });
+
+                //  console.log(query);
+
+
 //                  var result = $firebaseArray(query).$loaded().then(function (res) {
 //                      console.log(result);
 //                      
 //                  });
 //                  
 //                    result.$loaded().then(function () {
-                
-                
-                
-        
+
+
+
+
 
 //                console.log('snapshot');
 //                console.log(searchText);
