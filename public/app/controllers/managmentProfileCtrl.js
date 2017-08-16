10a255a4880c64d91ffc4a58e82c7f7aa35c7b33
avner ainouz <aainouz@univ.haifa.ac.il>
2017-08-16 23:27:53 +0300

    fix profile form validation for all fields + ng-hide when not active + fix first club object in club + fix upload picture + todo : match when club name new

--- public/app/controllers/managmentProfileCtrl.js
+++ public/app/controllers/managmentProfileCtrl.js
@@ -1,41 +1,43 @@
 angular
         .module('app')
-        .controller('managmentProfileCtrl', function ($scope, $timeout, CLUBES, $state, $stateParams, currentClub) {
+        .controller('managmentProfileCtrl', function ($scope, $timeout, $clubToast, $state, $stateParams, currentClub) {
             console.log(currentClub);
             $scope.club = currentClub;
 
             $scope.image2 = {};
-//            $scope.clubPicture = "img/empty-club.jpg";
-
-//            $scope.club.address = {};
-//            $scope.club.address.name = "";
-//            $scope.club.address.components = {placeId: "", streetNumber: "", street: "", city: "", state: "", countryCode: "", country: "", postCode: ""};
-
-//            $scope.club.address.components.location = {lat: "", long: ""};
-
             $scope.check = function () {
                 console.log($scope.club.address);
             };
 
             $scope.updateClub = function () {
-                console.log($scope.clubForm.$valid); 
+                console.log($scope.clubForm.$valid);
                 if ($scope.clubForm.$valid) {
                     $scope.upload = true;
-                    console.log('in profile');
                     if (angular.equals($scope.image2, {})) {
-
-                        $scope.club.$save();
-                        console.log('if');
+                        $scope.club.active = true;
+                        var cTime = new Date();
+                        $scope.club.open = cTime.getTime();
+                        $scope.club.$save().then(function () {
+                            $clubToast.show('פרופיל המועדון עודכן', 'clubProfile', 'success');
+                        }, function (error) {
+                            $clubToast.show('חלה שגיאה בעדכון!', 'clubProfile', 'error');
+                            console.log(error);
+                        });
                     } else {
-                        console.log('else');
+
                         var imagesRef = firebase.storage().ref('clubes/' + $scope.club.$id + '/profile/profile.jpg');
                         imagesRef.putString($scope.image2.resized.dataURL, 'data_url').then(function (snapshot) {
 
-                            var status = 'Your image "' + $scope.image2.file.name + '" has been successfully uploaded!';
-                            console.log(status);
-
-                            $scope.club.clubPicture = snapshot.metadata.downloadURLs[0]
-                            $scope.club.$save();
+                            $scope.club.clubPicture = snapshot.metadata.downloadURLs[0];
+                            $scope.club.active = true;
+                            var cTime = new Date();
+                            $scope.club.open = cTime.getTime();
+                            $scope.club.$save().then(function () {
+                                $clubToast.show('פרופיל המועדון עודכן', 'clubProfile', 'success');
+                            }, function (error) {
+                                $clubToast.show('חלה שגיאה בעדכון!', 'clubProfile', 'error');
+                                console.log(error);
+                            });
                             $scope.image2.file = undefined;
                             $scope.image2.url = undefined;
                             $scope.image2.dataURL = undefined;
@@ -43,13 +45,10 @@ angular
                             $scope.image2.resized.type = undefined;
 
                         }), function (error) {
-                            error = 'There was an error while uploading your image: ' + error;
+                            $clubToast.show('חלה שגיאה בהעלאת התמונה!', 'clubProfile', 'error');
                             console.log(error);
                         };
                     }
-
-
-//                CLUBES.updateClub($scope.club,  $scope.image2);
                     $scope.upload = false;
                 }
             };
@@ -64,6 +63,34 @@ angular
                     $scope.clubPicture = "img/empty-club.jpg";
             });
 
+            $scope.$watch('club.address.city', function () {
+                $timeout(function () {
+                    $scope.clubForm.address.$setValidity("city", $scope.club.address.city !== undefined && $scope.club.address.city !== "");
+                    $scope.$apply();
+                });
+            });
+
+            $scope.$watch('club.address.street', function () {
+                $timeout(function () {
+                    $scope.clubForm.address.$setValidity("street", $scope.club.address.street !== undefined && $scope.club.address.street !== "");
+                    $scope.$apply();
+                });
+            });
+
+            $scope.$watch('club.address.country', function () {
+                $timeout(function () {
+                    $scope.clubForm.address.$setValidity("country", $scope.club.address.country !== undefined && $scope.club.address.country !== "");
+                    $scope.$apply();
+                });
+            });
+
+            $scope.$watch('club.address.streetNumber', function () {
+                $timeout(function () {
+                    $scope.clubForm.address.$setValidity("streetNumber", $scope.club.address.streetNumber !== undefined && $scope.club.address.streetNumber !== "");
+                    $scope.$apply();
+                });
+            });
+
             $scope.goParties = function () {
                 $state.go('managment.parties', {clubId: $stateParams.clubId, role: $stateParams.role});
             };
@@ -72,4 +99,3 @@ angular
                 $state.go('managment.profile', {clubId: $stateParams.clubId, role: $stateParams.role});
             };
         });
-      
--- public/app/services/clubServ.js
+++ public/app/services/clubServ.js
@@ -21,44 +21,12 @@
                             description: null
                         };
                         clubObj.address.name = "";
-                        clubObj.address = {placeId: "", streetNumber: "", street: "", city: "", state: "", countryCode: "", country: "", postCode: ""};
+                        clubObj.address = {placeId: "", streetNumber: "", street: "", city: "", state: "", countryCode: "", country: ""};
                         clubObj.location = {lat: "", long: ""};
 
                         var newClub = clubRef.push(clubObj);
                         return newClub;
 
-                    };
-
-                    this.updateClub = function (club, image2) {
-
-                        console.log(club);
-                        console.log(image2);
-                        console.log('imagesRef');
-                        console.log(imagesRef);
-                        if (angular.equals(image2, {})) {
-                            
-                            club.$save();
-                            console.log(club);
-                        } else {
-
-                            var imagesRef = firebase.storage().ref('clubes/' + club.$id + '/profile/profile.jpg');
-                            imagesRef.putString(image2.resized.dataURL, 'data_url').then(function (snapshot) {
-
-                                status = 'Your image "' + image2.file.name + '" has been successfully uploaded!';
-                                console.log(snapshot);
-//                            image2.file = undefined;
-//                            image2.url = undefined;
-//                            image2.dataURL = undefined;
-//                            image2.resized.dataURL = undefined;
-//                            image2.resized.type = undefined;
-                                club.clubPicture = snapshot.metadata.downloadURLs[0]
-                                club.$save();
-
-                            }), function (error) {
-                                error = 'There was an error while uploading your image: ' + error;
-                                console.log(error);
-                            };
-                        }
                     };
 
                     this.getClubesUserAssign = function (clubes) {
@@ -84,11 +52,18 @@
 
 
                     this.GetOneClub = function (clubKey) {
-                        return $firebaseObject(ClubesRef.child(clubKey));
+                        var one = $q.defer();
+                        var club = $firebaseObject(ClubesRef.child(clubKey));
+
+                        club.$loaded().then(function () {
+                            one.resolve(club);
+                        });
+                        // Avner remmber that you didn't handle errores in load userr object. For later add catch
+                        return one.promise;
                     };
 
-             
                 }
                 ;
+
             });
 })(); 
