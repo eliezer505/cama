74cccea6c885865fa05a63d0e0eafb56dd4d6db8
avner ainouz <aainouz@univ.haifa.ac.il>
2018-02-06 21:25:38 +0200

    upload again

--- public/app/directives/scrollHeadlineClubes.js
+++ public/app/directives/scrollHeadlineClubes.js
@@ -1,21 +1,29 @@
 angular.module('app')
 
-        .directive("scrollHeadlinePending", function ($window) {
+        .directive("scrollHeadlineClubes", function ($window) {
             return function (scope, element, attrs) {
-                var header = document.querySelector('#pending-toolbar');
-                var content = document.querySelector('#pending-content');
-                var last = document.querySelector('.club-hr-no-bottom');
+                var header = document.querySelector('#clubes-toolbar');
+                var content = document.querySelector('#clubes-content');
+                var last = document.querySelector('.club-hr-bottom');
 
+                console.log(header);
+                console.log(content);
+                console.log(last);
                 element.bind('scroll', function () {
-                    last = document.querySelector('.club-hr-no-bottom');
+                    last = document.querySelector('.club-hr-bottom');
 
                     var recTool = header.getBoundingClientRect();
                     var recCont = content.getBoundingClientRect();
                     var recLast = last.getBoundingClientRect();
+                    console.log(recTool);
+                    console.log(recCont);
+                    console.log(recLast);
 
                     if (recCont.bottom >= recLast.top)
+
                     {
-                        scope.users.$scroll();
+                        console.log('in bottom');
+                        scope.clubes.$scroll();
                     }
 
                     if (recTool.bottom !== recCont.top) {
